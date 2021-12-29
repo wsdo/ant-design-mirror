938e110b2ef202ecc32ae184e91533596c12962f
zombieJ <zombieJ@users.noreply.github.com>
2021-12-29 13:40:24 +0000

    deploy: 3913fd51fbd18eeab1c8f6a724bfb4c7e1396e99

---
+++ components/rate/demo.e75655.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["components/rate/demo"],{
+
+/***/ "./node_modules/bisheng/tmp/components-rate-demo.index.js":
+/*!****************************************************************!*\
+  !*** ./node_modules/bisheng/tmp/components-rate-demo.index.js ***!
+  \****************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = {\n    'basic': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/rate/demo/basic.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/rate/demo/basic.md\"),\n    'character-function': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/rate/demo/character-function.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/rate/demo/character-function.md\"),\n    'character': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/rate/demo/character.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/rate/demo/character.md\"),\n    'clear': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/rate/demo/clear.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/rate/demo/clear.md\"),\n    'disabled': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/rate/demo/disabled.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/rate/demo/disabled.md\"),\n    'half': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/rate/demo/half.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/rate/demo/half.md\"),\n    'text': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/rate/demo/text.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/rate/demo/text.md\"),\n}\n\n//# sourceURL=webpack:///./node_modules/bisheng/tmp/components-rate-demo.index.js?");
+
+/***/ })
+
+}]);
