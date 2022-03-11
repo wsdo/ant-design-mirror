36071e5f4bb773b62b34c6372a9259d32a33f472
zombiej <smith3816@gmail.com>
2022-03-11 14:40:31 +0800

    chore: init

--- components/menu/style/index.tsx
+++ components/menu/style/index.tsx
@@ -1,6 +1,45 @@
-import '../../style/index.less';
-import './index.less';
+// import '../../style/index.less';
+// import './index.less';
 
-// style dependencies
-// deps-lint-skip: layout
-import '../../tooltip/style';
+// // style dependencies
+// // deps-lint-skip: layout
+// import '../../tooltip/style';
+
+// deps-lint-skip-all
+import {
+  DerivativeToken,
+  useStyleRegister,
+  useToken,
+  UseComponentStyleResult,
+  GenerateStyle,
+} from '../../_util/theme';
+
+interface MenuToken extends DerivativeToken {
+  menuCls: string;
+}
+
+// =============================== Base ===============================
+const genBaseStyle: GenerateStyle<MenuToken> = token => {
+  const { menuCls } = token;
+
+  return {
+    [menuCls]: {},
+  };
+};
+
+// ============================== Export ==============================
+export default function useStyle(prefixCls: string): UseComponentStyleResult {
+  const [theme, token, hashId] = useToken();
+
+  const cascaderToken: MenuToken = {
+    ...token,
+    menuCls: `.${prefixCls}`,
+  };
+
+  return [
+    useStyleRegister({ theme, token, hashId, path: [prefixCls] }, () => [
+      genBaseStyle(cascaderToken),
+    ]),
+    hashId,
+  ];
+}
