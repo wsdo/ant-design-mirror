9af26686732e0963805c2a6265822a409b02fc32
afc163 <afc163@gmail.com>
2022-04-19 10:28:32 +0800

    Deploy to gh-pages - 10:28:17

---
+++ 0.735367.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./node_modules/docsearch-react-fork/modal.js":
+/*!****************************************************!*\
+  !*** ./node_modules/docsearch-react-fork/modal.js ***!
+  \****************************************************/
+/*! exports provided: DocSearchModal */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dist_esm_DocSearchModal_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dist/esm/DocSearchModal.js */ \"./node_modules/docsearch-react-fork/dist/esm/DocSearchModal.js\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"DocSearchModal\", function() { return _dist_esm_DocSearchModal_js__WEBPACK_IMPORTED_MODULE_0__[\"DocSearchModal\"]; });\n\n\n\n\n//# sourceURL=webpack:///./node_modules/docsearch-react-fork/modal.js?");
+
+/***/ })
+
+}]);
---
+++ docs/spec/research-workbench.en-US.md.397ae0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[300],{3181:function(e,t){e.exports={content:["article"],meta:{category:"Design Patterns (Research)",type:"Template Document",order:2,title:"Workbench",skip:!0,filename:"docs/spec/research-workbench.en-US.md"},toc:["ul"]}}}]);
