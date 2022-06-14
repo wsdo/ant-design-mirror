7454fdb94b9dc765d12c644166ea33d1c9665836
MadCcc <MadCcc@users.noreply.github.com>
2022-06-14 18:03:14 +0000

    deploy: 25e1d1bc77c4be6ef2a3bb00917b9b6b03893407

---
+++ components/comment/demo.b4ba3d.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["components/comment/demo"],{
+
+/***/ "./node_modules/bisheng/tmp/components-comment-demo.index.js":
+/*!*******************************************************************!*\
+  !*** ./node_modules/bisheng/tmp/components-comment-demo.index.js ***!
+  \*******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = {\n    'basic': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/comment/demo/basic.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/comment/demo/basic.md\"),\n    'editor': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/comment/demo/editor.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/comment/demo/editor.md\"),\n    'list': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/comment/demo/list.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/comment/demo/list.md\"),\n    'nested': __webpack_require__(/*! ./node_modules/bisheng/lib/loaders/source-loader!./components/comment/demo/nested.md */ \"/home/runner/work/ant-design/ant-design/node_modules/bisheng/lib/loaders/source-loader!/home/runner/work/ant-design/ant-design/components/comment/demo/nested.md\"),\n}\n\n//# sourceURL=webpack:///./node_modules/bisheng/tmp/components-comment-demo.index.js?");
+
+/***/ })
+
+}]);
