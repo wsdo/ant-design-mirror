5804a2aaa36eb1ce8f35953176f31aa9bfeb7e3e
zombieJ <zombieJ@users.noreply.github.com>
2022-07-27 10:05:29 +0000

    deploy: c231e139cdfd83842e47d48831fc8f7efcb276d1

--- components/page-header/demo.56f006.js
+++ components/page-header/demo.56f006.js
@@ -1139,4 +1139,4 @@ tr:last-child td {
 @media (max-width: 576px) {
   #components-page-header-demo-responsive .content {
     display: block;
-  }`,src:"/demo-0.6297756091280544.html"}}}]);
+  }`,src:"/demo-0.3011010931088449.html"}}}]);
